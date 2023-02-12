3b7c93296a36a96686c7ab3425b6672f5610bb90
dancun616 <dancun616@gmail.com>
2023-02-12 10:22:28 +0300

    complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(new_word)
+        new_word.select do |w|
+            w.split(//).sort == word.split(//).sort
+        end
+    end
+end
